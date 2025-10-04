3bd847bf1293c6a71a9da8b935b89e5999a990f3
CagesThrottleUs <manstein.felix@gmail.com>
2025-10-04 21:17:17 +0530

    feat: integrate Adobe React Spectrum for theming
    - Added the `Provider` component from `@adobe/react-spectrum` to wrap the `App` component, enabling theming support.
    - Updated import statements for consistency and clarity.
    
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { defaultTheme, Provider } from "@adobe/react-spectrum";
 
-createRoot(document.getElementById('root')!).render(
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <Provider theme={defaultTheme}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
