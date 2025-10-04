090f71255d8e5a618da8e44566e64fd27c840cb6
CagesThrottleUs <manstein.felix@gmail.com>
2025-10-04 22:28:46 +0530

    feat: add Footer component with styling
    - Introduced a new `FooterComponent` using Adobe React Spectrum for consistent theming.
    - Created accompanying CSS styles for the footer to ensure proper positioning and alignment.
    - The footer includes a copyright notice and is styled to fit the overall application design.
    
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,22 @@
+import { Footer, View } from "@adobe/react-spectrum";
+import "./Footer.css";
+
+function FooterComponent() {
+  return (
+    <View
+      UNSAFE_className="app-footer"
+      borderWidth="thin"
+      borderBottomWidth="none"
+      borderXWidth="none"
+      width="100%"
+      backgroundColor="gray-200"
+      colorVersion={6}
+    >
+      <Footer UNSAFE_className="text-footer">
+        Built with ❤️ by CagesThrottleUs. &copy; 2025. &nbsp;
+      </Footer>
+    </View>
+  );
+}
+
+export default FooterComponent;
