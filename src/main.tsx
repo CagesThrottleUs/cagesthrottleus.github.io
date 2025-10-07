6e8a947cde8e4f1d71dfa99647aa006826d227ab
CagesThrottleUs <manstein.felix@gmail.com>
2025-10-07 17:11:12 +0530

    feat: integrate HashRouter for improved routing
    - Wrapped the application in a `HashRouter` to enable client-side routing.
    - This change enhances navigation capabilities within the app using `react-router`.
    
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

--- src/main.tsx
+++ src/main.tsx
@@ -3,16 +3,19 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { darkTheme, Provider } from "@adobe/react-spectrum";
+import { HashRouter } from "react-router";
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider
-      theme={darkTheme}
-      UNSAFE_className="app text-mono"
-      id="app-provider"
-    >
-      <App />
-    </Provider>
+    <HashRouter>
+      <Provider
+        theme={darkTheme}
+        UNSAFE_className="app text-mono"
+        id="app-provider"
+      >
+        <App />
+      </Provider>
+    </HashRouter>
   </StrictMode>
 );
