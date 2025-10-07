758fdbadbeae72d1b90c3a6a28cee89ba3d71175
CagesThrottleUs <manstein.felix@gmail.com>
2025-10-07 12:04:58 +0530

    feat: implement Header component with styling
    - Added a new `HeaderComponent` to the application, featuring a responsive layout with application name and version display.
    - Created accompanying CSS styles for the header, including typography and link effects for improved user experience.
    
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,52 @@
+import { Flex, Heading, Link, View, Text } from "@adobe/react-spectrum";
+import "./Header.css";
+import { APPLICATION_VERSION, SUPPORTED_APPLICATIONS } from "./constants";
+
+function HeaderComponent() {
+  return (
+    <View
+      UNSAFE_className="app-header"
+      backgroundColor="gray-300"
+      colorVersion={6}
+      padding="size-200"
+    >
+      <Flex
+        UNSAFE_className="text-header"
+        direction="row"
+        gap="size-400"
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Flex
+          direction="row"
+          gap="size-100"
+          justifyContent="start"
+          // alignItems="center"
+        >
+          <Link href="/" UNSAFE_className="link-header-no-effect">
+            <Heading level={1} marginY="auto">
+              cagesthrottleus
+            </Heading>
+          </Link>
+          <Text marginTop="size-200">v{APPLICATION_VERSION}</Text>
+        </Flex>
+        <Flex
+          direction="row"
+          gap="size-500"
+          justifyContent="end"
+          alignItems="center"
+        >
+          {SUPPORTED_APPLICATIONS.map((application) => (
+            <Heading level={2} marginY="auto">
+              <Link href={application.url} UNSAFE_className="link-header">
+                {application.name}
+              </Link>
+            </Heading>
+          ))}
+        </Flex>
+      </Flex>
+    </View>
+  );
+}
+
+export default HeaderComponent;
