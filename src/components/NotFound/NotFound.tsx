53b3bf9b20b8f62fc9643fec737c7d41d3486ff3
CagesThrottleUs <manstein.felix@gmail.com>
2025-10-07 17:01:20 +0530

    feat: add NotFound component for 404 error handling
    - Implemented a new `NotFoundComponent` that displays a 404 error message with an illustration and a link to navigate back to the home page.
    - Utilized `@adobe/react-spectrum` for UI components and `react-router` for navigation.
    
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

---
+++ src/components/NotFound/NotFound.tsx
@@ -0,0 +1,27 @@
+import {
+  Content,
+  Heading,
+  IllustratedMessage,
+  Link,
+  View,
+} from "@adobe/react-spectrum";
+import NotFound from "@spectrum-icons/illustrations/NotFound";
+import { useNavigate } from "react-router";
+
+function NotFoundComponent() {
+  const navigate = useNavigate();
+  return (
+    <View paddingTop="38vh">
+      <IllustratedMessage>
+        <NotFound />
+        <Heading level={1}>Error: 404</Heading>
+        <Content>
+          Page not found. Return to{" "}
+          <Link onPress={() => void navigate("/")}>home</Link>
+        </Content>
+      </IllustratedMessage>
+    </View>
+  );
+}
+
+export default NotFoundComponent;
