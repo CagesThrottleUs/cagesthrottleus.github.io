97cac05a9503a267b7793a22c885ab48d6eba0e1
CagesThrottleUs <manstein.felix@gmail.com>
2025-10-07 17:11:53 +0530

    fix: adjust NotFound component layout for better visibility
    - Changed the height of the NotFound component to `80vh` and added a top margin of `10vh` to the IllustratedMessage for improved visual spacing.
    
    Signed-off-by: CagesThrottleUs <manstein.felix@gmail.com>

--- src/components/NotFound/NotFound.tsx
+++ src/components/NotFound/NotFound.tsx
@@ -11,8 +11,8 @@ import { useNavigate } from "react-router";
 function NotFoundComponent() {
   const navigate = useNavigate();
   return (
-    <View paddingTop="38vh">
-      <IllustratedMessage>
+    <View height="80vh">
+      <IllustratedMessage marginTop="10vh">
         <NotFound />
         <Heading level={1}>Error: 404</Heading>
         <Content>
